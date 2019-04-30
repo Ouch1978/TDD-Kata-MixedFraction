94cdfc4c6700e134854f85e5b51e38b7025f2c1e
Ouch.Liu <ouch.liu@xuenn.com>
2019-04-30 17:03:08 +0800

    1. initial commit

---
+++ MixedFraction/MixedFraction/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixedFraction
+{
+    public class Kata
+    {
+        public static string MixedFraction( string s )
+        {
+            return s;
+        }
+    }
+}
---
+++ MixedFraction/MixedFractionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "MixedFractionTest" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "MixedFractionTest" )]
+[assembly: AssemblyCopyright( "Copyright ©  2019" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "06f367e3-0b49-44cc-a33a-8838a8266727" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
